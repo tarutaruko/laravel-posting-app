40a9c563d1283d7f17446d64e12b481fadb58fb5
tarutaruko <tarutaruko39@gmail.com>
2025-08-10 23:35:56 +0900

    first commit

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Post::factory()->count(5)->create();
+      
+    }
+}
